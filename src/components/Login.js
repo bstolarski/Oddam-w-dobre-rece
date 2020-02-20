efcfe36b7b66c6744be05756090be090f31bc33c
bstolarski <b.stolarski@hotmail.com>
2020-02-20 16:13:47 +0100

    Login validation - in proggres

--- src/components/Login.js
+++ src/components/Login.js
@@ -15,6 +15,32 @@ export default class Login extends Component {
         })
     };
 
+    handleSubmit = e => {
+        e.preventDefault();
+        const inputValidation = {
+            email: /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+            password: /^(?=.{6,})$/
+        };
+        if (!inputValidation.email.test(this.state.email)) {
+            this.setState({
+                emailError: true
+            })
+        } else {
+            this.setState({
+                emailError: false
+            })
+        }
+        if (this.state.password.length < 6) {
+            this.setState({
+                passwordError: true
+            })
+        } else {
+            this.setState({
+                passwordError: false
+            })
+        }
+    };
+
     render() {
         return (
             <>
@@ -23,17 +49,21 @@ export default class Login extends Component {
                     <section className='login-container'>
                         <h2 className='login-title'>{this.props.title}</h2>
                         <div className='header-decoration'/>
-                        <form className='login-form'>
+                        <form className='login-form' onSubmit={this.handleSubmit}>
                             <div className='login-form-content'>
                                 <label className='login-input-label'>
                                     Email
                                     <input type="text" name="email" value={this.state.email}
                                            onChange={this.handleInputChange}/>
+                                    {this.state.emailError ?
+                                        <span className='contact-error'>Podany email jest nieprawidłowy</span> : ''}
                                 </label>
                                 <label className='login-input-label'>
                                     Hasło
                                     <input type="password" name="password" value={this.state.password}
                                            onChange={this.handleInputChange}/>
+                                    {this.state.passwordError ?
+                                        <span className='contact-error'>Podane hasło jest za krótkie</span> : ''}
                                 </label>
                                 {this.props.repeatPassword ?
                                     <label className='login-input-label'>
