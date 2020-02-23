4621de7cbe4e072f7567e998d14e60119fb99066
bstolarski <b.stolarski@hotmail.com>
2020-02-23 17:08:43 +0100

    Give stuff header - done

--- src/components/Form/FormHeader.js
+++ src/components/Form/FormHeader.js
@@ -2,17 +2,21 @@ import React, {Component} from 'react';
 import {HomeHeaderMenu} from "../HomeHeader/HomeHeaderMenu";
 import {FormHeaderNav} from "./FormHeaderNav";
 import {FormHeaderMain} from "./FormHeaderMain";
+import FormHeaderImportant from "./FormHeaderImportant";
 
 export default class FormHeader extends Component {
     render() {
         return (
-            <header name='start' className='form-header-container' id={'start-page'}>
-                <div className='form-header-img'/>
-                <div className='form-header-nav'>
-                    <FormHeaderNav/>
-                    <HomeHeaderMenu/>
-                    <FormHeaderMain/>
+            <header className="form-header">
+                <div name='start' className='form-header-container' id='start-page'>
+                    <div className='form-header-img'/>
+                    <div className='form-header-nav'>
+                        <FormHeaderNav/>
+                        <HomeHeaderMenu/>
+                        <FormHeaderMain/>
+                    </div>
                 </div>
+                <FormHeaderImportant/>
             </header>
         )
     }
