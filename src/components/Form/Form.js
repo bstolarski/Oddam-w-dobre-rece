b996fc16082370c0acf0ec3e45f6159b0b185dbf
bstolarski <b.stolarski@hotmail.com>
2020-02-23 17:50:40 +0100

    Give stuff steps - in proggress

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import FormHeader from "./FormHeader";
 import GiveStuff from "./GiveStuff/GiveStuff";
+import {HomeContact} from "../HomeContact/HomeContact";
 
 export default class Form extends Component {
     render() {
@@ -9,6 +10,7 @@ export default class Form extends Component {
                 <div className='container'>
                     <FormHeader/>
                     <GiveStuff/>
+                    <HomeContact/>
                 </div>
             </>
         )
--- src/components/Form/GiveStuff/GiveStuffStep1.js
+++ src/components/Form/GiveStuff/GiveStuffStep1.js
@@ -1,6 +1,15 @@
 import React, {Component} from 'react';
 
 export default class GiveStuffStep1 extends Component {
+    state = {
+        currentSelect: "1"
+    };
+
+    handleSelectChange = (e) => {
+        this.setState({
+            currentSelect: e.target.value
+        })
+    };
 
     render() {
         return (
@@ -10,8 +19,51 @@ export default class GiveStuffStep1 extends Component {
                     <div className='give-stuff-step-box-inputs'>
                         <h3>Zaznacz co chcesz oddać:</h3>
                         <div className='box-input-container'>
-
-
+                            <label
+                                className={this.state.currentSelect === '1' ? 'btn-choose-label checked' : 'btn-choose-label'}>
+                                <input className='btn-choose-input'
+                                       type="radio"
+                                       name="radio"
+                                       value="1"
+                                       onClick={this.handleSelectChange}
+                                /> ubrania, które nadają się do ponownego użycia
+                            </label>
+                            <label
+                                className={this.state.currentSelect === '2' ? 'btn-choose-label checked' : 'btn-choose-label'}>
+                                <input className='btn-choose-input'
+                                       type="radio"
+                                       name="radio"
+                                       value="2"
+                                       onClick={this.handleSelectChange}
+                                /> ubrania, do wyrzucenia
+                            </label>
+                            <label
+                                className={this.state.currentSelect === '3' ? 'btn-choose-label checked' : 'btn-choose-label'}>
+                                <input className='btn-choose-input'
+                                       type="radio"
+                                       name="radio"
+                                       value="3"
+                                       onClick={this.handleSelectChange}
+                                /> zabawki
+                            </label>
+                            <label
+                                className={this.state.currentSelect === '4' ? 'btn-choose-label checked' : 'btn-choose-label'}>
+                                <input className='btn-choose-input'
+                                       type="radio"
+                                       name="radio"
+                                       value="4"
+                                       onClick={this.handleSelectChange}
+                                /> książki
+                            </label>
+                            <label
+                                className={this.state.currentSelect === '5' ? 'btn-choose-label checked' : 'btn-choose-label'}>
+                                <input className='btn-choose-input'
+                                       type="radio"
+                                       name="radio"
+                                       value="5"
+                                       onClick={this.handleSelectChange}
+                                /> inne
+                            </label>
                         </div>
                     </div>
                 </div>
